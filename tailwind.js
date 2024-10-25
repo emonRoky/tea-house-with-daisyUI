ea9a735a9d290d943b1af1502e7fb4a57d4975ac
Roky <apple@Rokys-MacBook-Air.local>
2024-10-25 21:22:14 +0900

    project setup

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          "bike-primary": '#E76F51',
+          "bike-primary-bg": 'rgba(231, 111, 81, 0.10)',
+        }
+      }
+    }
+  }
